d965467b24bbd455fa023bfa84ad380bd99fba2f
fafnur <fafnur@yandex.ru>
2015-12-27 20:00:58 +0600

    start decomposition gulp file

--- gulp/build.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var path  = require('path'),
-    gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    conf  = require('./config');
---
+++ gulp/css/less.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+ * Compile LESS files
+ */
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    conf = require('../config');
+
+var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'sourcemaps', 'browser-sync']
+});
+
+gulp.task(conf.preCSS.task, function () {
+    return gulp.src(conf.preCSS.src)
+        .pipe($.concat(conf.preCSS.in))
+        .pipe($.gulpIf(conf.preCSS.isSourcemaps, $.sourcemaps.init()))
+        .pipe($.less())
+        .on('error', $.notify.onError(function (error) {
+            return '\nError! Look in the console for details.\n' + error;
+        }))
+        .pipe($.rename(conf.preCSS.out))
+        .pipe($.gulpIf(conf.preCSS.isSourcemaps, $.sourcemaps.write()))
+        .pipe(gulp.dest(conf.htdocs.css))
+        .pipe($.browserSync.reload({stream: true}));
+});
